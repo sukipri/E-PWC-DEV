99cd60659d91a6db7df2ddcb99eefd2d609bb1eb
sukipri <sukipri9985@gmail.com>
2024-10-03 09:17:25 +0700

    CMT

--- DFMBL_V3/DIST/CODE/CODE_02.php
+++ DFMBL_V3/DIST/CODE/CODE_02.php
@@ -1,22 +1,4 @@
 <?php
-            /* E-PWC */
-    /*-------------------------------------------------------*/
-    /* Himpunan Data Code Variabel DAN FUNGSI  QUERING DBMS */
-    /*-----------------------------------------------------*/
-
-
-    /*_________________________________________________*/
-
-        /* Numbering Field Code Quering 
-                // titik_kode_01
-                $ec_cn_vtt01_sw = $CL_Q("$SL titik_kode_01 FROM tb_ec_titik_01 ");
-                $ec_nr_vtt01_cq = $CL_NR($ec_cn_vtt01_sw);
-                $ec_hit_vtt01_cq = $ec_nr_vtt01_cq + 1;
-                $ec_hit_zero_vtt01_cq = sprintf("%02d", $ec_hit_vtt01_cq);
-                @$ec_cq_vtt01_sw = "TT$DATE_ymd-$ec_hit_zero_vtt01_cq"; 
-                // titik_kode_01
-               */
-  
 
         //--GET VIEW--//
               $IDRM01 = @$SQL_SL($_GET['IDRM01']); #NOMOR REKAM MEDIS
